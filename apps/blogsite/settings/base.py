86875f593b66cd6239a6988e73901b75656081a9
Gunnar Scherf <mail@gunnar-scherf.de>
2023-03-08 21:49:22 +0100

    recaptcha2

--- apps/blogsite/settings/base.py
+++ apps/blogsite/settings/base.py
@@ -256,7 +256,6 @@ CSRF_REQUIRED_PATHS = (
     "/admin",
     "/django-admin",
 )
-RECAPTCHA_REQUIRED_SCORE = float(os.getenv('RECAPTCHA_REQUIRED_SCORE', "0.85"))
 
 # mozilla-django-oidc settings
 LOGIN_REDIRECT_URL = "/admin"
