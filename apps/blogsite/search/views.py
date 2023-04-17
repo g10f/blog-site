f984ec51e1913b8ddd886a71138085dfc464894f
Gunnar Scherf <mail@gunnar-scherf.de>
2023-04-17 20:10:13 +0200

    search site specific

--- apps/blogsite/search/views.py
+++ apps/blogsite/search/views.py
@@ -1,18 +1,20 @@
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.template.response import TemplateResponse
 from django.utils.translation import gettext_lazy as _
 
-from wagtail.models import Page
+from wagtail.models import Page, Site
 from wagtail.search.models import Query
 
 
 def search(request):
     search_query = request.GET.get('q', None)
     page = request.GET.get('page', 1)
+    site = Site.find_for_request(request)
 
     # Search
     if search_query:
-        search_results = Page.objects.live().search(search_query)
+        search_results = Page.objects.live().in_site(site).search(search_query)
         query = Query.get(search_query)
 
         # Record hit
