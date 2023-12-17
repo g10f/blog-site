0669f2bfe3f994b3a0602a671909a0008cab85de
Gunnar Scherf <mail@gunnar-scherf.de>
2023-12-17 13:31:17 +0100

    bootstrap-icons": "^1.11.2",

---
+++ set-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+VERSION=1.4.6
+
+sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" apps/blogsite/__init__.py
+
+sed -i "s/FROM ghcr\.io\/g10f\/blog-site:.*/FROM ghcr\.io\/g10f\/blog-site:${VERSION}/" ../blog-michal-theme/Dockerfile
+sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" ../blog-michal-theme/michal_theme/__init__.py
+
+sed -i "s/FROM ghcr\.io\/g10f\/blog-site:.*/FROM ghcr\.io\/g10f\/blog-site:${VERSION}/" ../afd-blog/Dockerfile
+sed -i "s/__version__ =.*/__version__ = '${VERSION}'/" ../afd-blog/afd_theme/__init__.py
+
