88cf38688d82dfb41960c8d5b47d74e852d91008
Gunnar Scherf <mail@gunnar-scherf.de>
2026-08-22 21:06:05 +0200

    wagtail 7.4.3

--- apps/blogsite/base/models.py
+++ apps/blogsite/base/models.py
@@ -305,7 +305,7 @@ class HomePage(Page):
     # Promo section of the HomePage
     promo_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+', help_text='Promo image')
     promo_title = models.CharField(null=True, blank=True, max_length=255, help_text='Title to display above the promo copy')
-    promo_text = RichTextField(null=True, blank=True, help_text='Write some promotional copy')
+    promo_text = RichTextField(null=True, blank=True, help_text='Write some promotional copy', features=['bold', 'italic', 'ol', 'ul', 'hr', 'document-link', 'link', 'button-link'])
 
     # Featured sections on the HomePage
     # You will see on templates/base/home_page.html that these are treated
--- apps/blogsite/base/wagtail_hooks.py
+++ apps/blogsite/base/wagtail_hooks.py
@@ -1,5 +1,17 @@
+from django.urls import reverse_lazy
+
 from blogsite.base.views import SiteFieldSnippetViewSet
+from draftjs_exporter.dom import DOM
+from wagtail import hooks
+from wagtail.admin.rich_text.converters.contentstate import link_entity
+from wagtail.admin.rich_text.converters.html_to_contentstate import (
+    ExternalLinkElementHandler,
+    PageLinkElementHandler,
+)
+from wagtail.admin.rich_text.editors.draftail import features as draftail_features
+from wagtail.rich_text import LinkHandler
 from wagtail.snippets.models import register_snippet
+from wagtail.whitelist import check_url
 
 
 class SpeakerViewSet(SiteFieldSnippetViewSet):
@@ -26,3 +38,72 @@ register_snippet("blogsite.base.models.Speaker", viewset=SpeakerViewSet)
 register_snippet("blogsite.base.models.People", viewset=PeopleViewSet)
 register_snippet("blogsite.base.models.SiteLogo", viewset=LogoViewSet)
 register_snippet("blogsite.base.models.FooterText", viewset=FooterViewSet)
+
+
+class ButtonLinkHandler(LinkHandler):
+    identifier = 'button'
+
+    @classmethod
+    def expand_db_attributes(cls, attrs):
+        id_ = attrs.get('id')
+        if id_ is not None:
+            try:
+                from wagtail.models import Page
+                page = Page.objects.get(pk=id_).specific
+                href = page.full_url
+            except Exception:
+                href = '#'
+        else:
+            href = attrs.get('href', '')
+        return f'<a href="{href}" class="btn btn-primary">'
+
+
+def button_link_entity(props):
+    id_ = props.get('id')
+    link_props = {'linktype': 'button'}
+    if id_ is not None:
+        link_props['id'] = id_
+    else:
+        link_props['href'] = check_url(props.get('url', ''))
+    return DOM.create_element('a', link_props, props['children'])
+
+
+@hooks.register('register_rich_text_features')
+def register_button_link_feature(features):
+    feature_name = 'button-link'
+    type_ = 'BUTTON_LINK'
+
+    features.register_link_type(ButtonLinkHandler)
+
+    features.register_editor_plugin(
+        'draftail', feature_name,
+        draftail_features.EntityFeature(
+            {
+                'type': type_,
+                'icon': 'link',
+                'description': 'Button',
+                'attributes': ['url', 'id', 'parentId'],
+                'allowlist': {
+                    'href': '^(http:|https:|mailto:|#|undefined$)',
+                },
+                'chooserUrls': {
+                    'pageChooser': reverse_lazy('wagtailadmin_choose_page'),
+                    'externalLinkChooser': reverse_lazy('wagtailadmin_choose_page_external_link'),
+                    'emailLinkChooser': reverse_lazy('wagtailadmin_choose_page_email_link'),
+                    'phoneLinkChooser': reverse_lazy('wagtailadmin_choose_page_phone_link'),
+                    'anchorLinkChooser': reverse_lazy('wagtailadmin_choose_page_anchor_link'),
+                },
+            },
+            js=['wagtailadmin/js/page-chooser-modal.js'],
+        )
+    )
+
+    features.register_converter_rule('contentstate', feature_name, {
+        'from_database_format': {
+            'a[linktype="button"][href]': ExternalLinkElementHandler(type_),
+            'a[linktype="button"][id]': PageLinkElementHandler(type_),
+        },
+        'to_database_format': {
+            'entity_decorators': {type_: button_link_entity}
+        }
+    })
